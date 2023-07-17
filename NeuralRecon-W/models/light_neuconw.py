b94b64bb61d8fca2d7d0c732c4791e40bf1dda37
YifanYang <xcnryyf@163.com>
2023-07-17 13:46:57 +0000

    create a template for NeuconW with light code,named 'LightNeuconW' on 7.17

---
+++ NeuralRecon-W/models/light_neuconw.py
@@ -0,0 +1,14 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from models.neuconw import *
+
+class LightNetwork(nn.Module):
+    def __init__(self, init_val):
+        super().__init__()
+      
+
+class LightNeuconW(nn.Module):
+    def __init__(self):
+        pass
