82f50f36b6201698399c5c740c24ae21a1905c1e
YifanYang <xcnryyf@163.com>
2023-07-25 09:09:29 +0000

    load a checkpoint,continue training

--- NeuralRecon-W/scripts/custom_train.sh
+++ NeuralRecon-W/scripts/custom_train.sh
@@ -15,5 +15,7 @@ cp ${config_file} logs/${jobname}
 export CUDA_VISIBLE_DEVICES=0
 python custom_train.py --cfg_path ${config_file} \
   --num_gpus 1 --num_nodes 1 \
+  --ckpt_path ckpts/train-light_test-20230723_144459/{epoch:d}/epoch=0-step=189999.ckpt \
   --num_epochs 20 --batch_size 2048 --test_batch_size 512 --num_workers 16 \
   --exp_name ${jobname} 2>&1|tee log/${jobname}.log \
+  
